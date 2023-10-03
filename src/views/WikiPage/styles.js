f7d9d34cfd4417b55ef9357dda71edad8f84184b
Syed Mudasser Anayat <107185992+MD6966@users.noreply.github.com>
2023-10-03 16:47:57 +0500

    New additions in Wiki page

--- src/views/WikiPage/styles.js
+++ src/views/WikiPage/styles.js
@@ -1,4 +1,4 @@
-import { Box, styled } from "@mui/material";
+import { Box, Card, styled } from "@mui/material";
 
 export const StyledRoot = styled(Box)(({theme})=> ({
     minHeight:'100vh',
@@ -8,4 +8,23 @@ export const StyledRoot = styled(Box)(({theme})=> ({
 export const StyledContent = styled(Box)(({theme})=>({
     padding:theme.spacing(4),
     minHeight:'50vh'
+}))
+
+ export const StyledProjectRoot = styled(Box)(({theme})=>({
+    paddingLeft:theme.spacing(15),
+    paddingRight:theme.spacing(15),
+    minHeight:'80vh'
+}))
+
+export const StyledProjectCard = styled(Card)(({theme})=>({
+    padding:theme.spacing(3)
+}))
+
+export const StyledProjectTop = styled (Box)(({theme})=>({
+    height:'25vh'
+}))
+
+export const StyledProjectBottom = styled (Box)(({theme})=>({
+    height:'50vh',
+    marginTop:'2rem'
 }))
